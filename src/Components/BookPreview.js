bba9a4fe9142abc3ec2d8c5c2ab5856ec4ba4d54
aresnik11 <aresnik11@gmail.com>
2019-11-04 23:55:15 -0500

    added initial book css animation

--- src/Components/BookPreview.js
+++ src/Components/BookPreview.js
@@ -14,7 +14,52 @@ const BookPreview = (props) => {
                     pathname: `/books/${props.id}`,
                     state: { bookListObj: props.bookListObj }
                 }}>
-                    <h4>{props.title}</h4>
+                    {/* <h4>{props.title}</h4> */}
+                    <div class="component">
+                        <ul class="align">
+                            <li>
+                                <figure class='book'>
+
+                                    {/* Front */}
+
+                                    <ul class='hardcover_front'>
+                                        <li>
+                                            <img src={props.image} alt="" />
+                                        </li>
+                                        <li></li>
+                                    </ul>
+
+                                    {/* Pages */}
+
+                                    <ul class='page'>
+                                        <li></li>
+                                        <li>
+                                            <a class="btn" href="#">Download</a>
+                                        </li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+
+                                    {/* Back */}
+
+                                    <ul class='hardcover_back'>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                    <ul class='book_spine'>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                    <figcaption>
+                                        <h1>Fivera.net</h1>
+                                        <span>By Nikola Petrovic</span>
+                                        <p>Website dedicated to sharing resources</p>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+			</div>
                 </Link>
                 <button onClick={() => props.removeBookListBook(props.id, props.bookListObj.id)}>Remove from book list</button>
             </>
